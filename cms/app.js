cdb36e1b52ce709c2aae7347b624433ccc7b527f
xavioo <rucahoxavi@gmail.com>
2020-04-18 06:52:15 +0200

    first run on browser

--- cms/app.js
+++ cms/app.js
@@ -1,6 +1,17 @@
 const express=require('express');
 const app=express();
+const path=require('path');
 
+
+
+
+app.use(express.static(path.join(__dirname,'public')));
+
+
+
+app.get('/', (req,res)=>{
+    res.send('It works');
+});
 
 app.listen(4500,()=>{
     console.log('listening on port 4500');
