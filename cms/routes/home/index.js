0eb77fa87816fd87b63c6505617871153f7537cf
xavioo <rucahoxavi@gmail.com>
2020-04-22 04:58:54 +0200

    display categories on homepage

--- cms/routes/home/index.js
+++ cms/routes/home/index.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const router=express.Router();
 const Post  = require('../../models/Post');
+const Category = require('../../models/Category');
 
 
 router.all('/*',(req,res,next)=>{
@@ -12,9 +13,11 @@ router.all('/*',(req,res,next)=>{
 router.get('/',(req,res)=>{
 
 Post.find({}).then(posts=>{ 
-res.render('home/index',{posts: posts}); 
+    Category.find({}).then(categories=>{
+res.render('home/index',{posts: posts,categories:categories}); 
 });
 
+});
 });
 
 router.get('/post/:id',(req,res)=>{
